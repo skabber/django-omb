242a2ddebfa190fb8f53fde865b7e201debade86
Jay Graves <jay@skabber.com>
2008-09-26 23:16:59 -0600

    Made the urls named url patterns.

--- urls.py
+++ urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    (r'^follow/$', 'omb.views.follow'),
-    (r'^finish_follow/$', 'omb.views.finish_follow'),
-    (r'^postnotice/$', 'omb.views.postnotice'),
-    (r'^updateprofile/$', 'omb.views.updateprofile'),
+    url(r'^follow/$', 'omb.views.follow', name="omb_follow"),
+    url(r'^finish_follow/$', 'omb.views.finish_follow', name="omb_finish_follow"),
+    url(r'^postnotice/$', 'omb.views.postnotice', name="omb_post_notice"),
+    url(r'^updateprofile/$', 'omb.views.updateprofile', name="omb_update_profile"),
+    url(r'^(?P<username>[\w]+)/xrds/$', 'omb.views.xrds', name="omb_xrds"),
 )
