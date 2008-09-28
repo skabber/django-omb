8b8f70733b132082585eabe1d04fb8013e1a5ddc
Jay Graves <jay@skabber.com>
2008-09-28 15:08:39 -0600

    Added the authorization form

--- forms.py
+++ forms.py
@@ -3,3 +3,12 @@ from django import forms
 class RemoteSubscribeForm(forms.Form):
     username = forms.CharField(max_length=64, label="Username")
     profile_url = forms.CharField(label="OMB Compatable Profile URL")
+
+AUTHORIZE_CHOICES = (
+    ('on', 'Accept'),
+    ('off', 'Reject')
+)
+
+class AuthorizeForm(forms.Form):
+    token = forms.CharField(widget=forms.HiddenInput)
+    authorize_access = forms.ChoiceField(choices=AUTHORIZE_CHOICES,widget=forms.RadioSelect, required=False)
