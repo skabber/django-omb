5014aee334c04699454ba8e1340cc6f9b246b22a
Jay Graves <jay@skabber.com>
2008-09-29 23:53:12 -0600

    Made the omb urls more consistent

--- urls.py
+++ urls.py
@@ -3,8 +3,8 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     url(r'^follow/$', 'omb.views.follow', name="omb_follow"),
     url(r'^finish_follow/$', 'omb.views.finish_follow', name="omb_finish_follow"),
-    url(r'^postnotice/$', 'omb.views.post_notice', name="omb_post_notice"),
-    url(r'^updateprofile/$', 'omb.views.updateprofile', name="omb_update_profile"),
+    url(r'^post_notice/$', 'omb.views.post_notice', name="omb_post_notice"),
+    url(r'^update_profile/$', 'omb.views.updateprofile', name="omb_update_profile"),
     url(r'^(?P<username>[\w]+)/xrds/$', 'omb.views.xrds', name="omb_xrds"),
     
     url(r'^request_token/$', 'omb.views.omb_request_token', name="omb_request_token"),
