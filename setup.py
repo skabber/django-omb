2a19b4a618008fb5e6b0a3fa08c75c9f4a2355d5
Jay Graves <jay@skabber.com>
2009-01-25 07:43:37 -0700

    Updated the setup file with a bit more info.

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python
+from setuptools import setup, find_packages
 
-from distutils.core import setup
+version = '0.1.0'
 
-setup(name='omb',
-      version='.01',
-      description='OpenMicroblogging support for Django',
-      author='Jay Graves',
-      author_email='jay@skabber.com',
-      url='http://github.com/skabber/django-omb/tree/master',
-      packages=[],
-     )
+setup(
+    name='django-microblogging',
+    version=version,
+    description='OpenMicroblogging support for Django',
+    classifiers=[
+        "Framework :: Django",
+        "Programming Language :: Python",
+        "Environment :: Web Environment",
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    keywords='omb,microblogging,django',
+    author='Jay Graves',
+    author_email='jay@skabber.com',
+    url='http://github.com/skabber/django-omb/tree/master',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+)
+
 
